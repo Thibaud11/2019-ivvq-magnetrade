7d71cca9b2f177b1fd556756686fa97f2274846c
hugo.clems <hugoc.pro@gmail.com>
2019-03-14 14:12:17 +0100

    Tests bidons

--- src/test/java/fr/univtlse3/m2dl/magnetrade/UnitTest.java
+++ src/test/java/fr/univtlse3/m2dl/magnetrade/UnitTest.java
@@ -1,16 +1,21 @@
 package fr.univtlse3.m2dl.magnetrade;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.logging.Logger;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MagnetradeApplicationTests {
+public class UnitTest {
 
     @Test
-    public void contextLoads() {
+    public void unitTest() {
+        Logger.getAnonymousLogger().info("TEST UNIT");
+        Assert.assertTrue(true);
     }
 
 }
