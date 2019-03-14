4be89bc19abcc416548e4d258c04f04e7b5d80c2
hugo.clems <hugoc.pro@gmail.com>
2019-03-14 14:12:17 +0100

    Tests bidons

---
+++ src/test/java/fr/univtlse3/m2dl/magnetrade/TestIT.java
@@ -0,0 +1,22 @@
+package fr.univtlse3.m2dl.magnetrade;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.logging.Logger;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class TestIT {
+
+    @Test
+    public void inteTest() {
+        Logger.getAnonymousLogger().info("TEST INTE");
+        assertThat("testIntegration", true);
+    }
+
+}
