1f22fddee868d033cff006d46da218c59dbab89c
hugo.clems <hugoc.pro@gmail.com>
2019-03-14 14:45:16 +0100

    Séparations tests

--- src/test/java/fr/univtlse3/m2dl/magnetrade/FooBarIT.java
+++ src/test/java/fr/univtlse3/m2dl/magnetrade/FooBarIT.java
@@ -11,7 +11,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class TestIT {
+public class FooBarIT {
 
     @Test
     public void inteTest() {
