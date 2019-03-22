f2d1fdaf67de40960d9ece69993460762d138a68
hugo.clems <hugoc.pro@gmail.com>
2019-03-22 15:16:41 +0100

    Création Comment

---
+++ src/main/java/fr/univtlse3/m2dl/magnetrade/comment/Comment.java
@@ -0,0 +1,125 @@
+package fr.univtlse3.m2dl.magnetrade.comment;
+
+import fr.univtlse3.m2dl.magnetrade.User;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * Un commentaire laissé par un utilisateur sur un poste.
+ */
+@Entity
+public class Comment {
+
+    /**
+     * L'identifiant
+     */
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    /**
+     * Status du commentaire.
+     * true si le commentaire à déjà était édité, false sinon
+     */
+    @NotNull
+    private boolean isEdited;
+
+    /**
+     * Date de publication d'un commentaire.
+     */
+    @NotNull
+    private Date date;
+
+    /**
+     * Le contenu du commentaire.
+     */
+    @NotNull
+    private String text;
+
+    /**
+     * L'utilisateur qui commente.
+     */
+    @ManyToOne
+    private User commenter;
+
+    public Comment() {
+        // Empty
+    }
+
+    public Comment(boolean isEdited, Date date, String text, User commenter) {
+        this.isEdited = isEdited;
+        this.date = date;
+        this.text = text;
+        this.commenter = commenter;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public boolean isEdited() {
+        return isEdited;
+    }
+
+    public void setEdited(boolean edited) {
+        isEdited = edited;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getCommenter() {
+        return commenter;
+    }
+
+    public void setCommenter(User commenter) {
+        this.commenter = commenter;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return isEdited == comment.isEdited &&
+                Objects.equals(date, comment.date) &&
+                Objects.equals(text, comment.text) &&
+                Objects.equals(commenter, comment.commenter);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isEdited, date, text, commenter);
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", isEdited=" + isEdited +
+                ", date=" + date +
+                ", text='" + text + '\'' +
+                ", commenter=" + commenter +
+                '}';
+    }
+
+}
---
+++ src/test/java/fr/univtlse3/m2dl/magnetrade/comment/CommentTest.java
@@ -0,0 +1,90 @@
+package fr.univtlse3.m2dl.magnetrade.comment;
+
+import fr.univtlse3.m2dl.magnetrade.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentTest {
+
+    private Date d;
+    private User u;
+
+    @Before
+    public void init() {
+        this.d = new Date();
+        this.u = new User();
+    }
+
+    @Test
+    public void isEditedTest() {
+        Comment c = new Comment(true, this.d, "", this.u);
+        Assert.assertTrue(c.isEdited());
+    }
+
+    @Test
+    public void setEditedTest() {
+        Comment c = new Comment(false, this.d, "", this.u);
+        c.setEdited(true);
+        Assert.assertTrue(c.isEdited());
+    }
+
+    @Test
+    public void getDateTest() {
+        Comment c = new Comment(true, this.d, "", this.u);
+        Assert.assertEquals(this.d, c.getDate());
+    }
+
+    @Test
+    public void setDateTest() {
+        Comment c = new Comment();
+        c.setDate(this.d);
+        Assert.assertEquals(this.d, c.getDate());
+    }
+
+    @Test
+    public void getTextTest() {
+        Comment c = new Comment(true, this.d, "HUGO", this.u);
+        Assert.assertEquals("HUGO", c.getText());
+    }
+
+    @Test
+    public void setTextTest() {
+        Comment c = new Comment();
+        c.setText("HUGO");
+        Assert.assertEquals("HUGO", c.getText());
+    }
+
+    @Test
+    public void getCommenterTest() {
+        User u1 = new User();
+        Comment c = new Comment(true, this.d, "", u1);
+        Assert.assertEquals(u1, c.getCommenter());
+    }
+
+    @Test
+    public void setCommenterTest() {
+        User u1 = new User();
+        Comment c = new Comment();
+        c.setCommenter(u1);
+        Assert.assertEquals(u1, c.getCommenter());
+    }
+
+    @Test
+    public void equalsTest() {
+        Comment c1 = new Comment(false, this.d, "COUCOU", null);
+        Comment c2 = new Comment(false, this.d, "COUCOU", null);
+        Comment c3 = new Comment(true, this.d, "COUCOU", null);
+
+        Assert.assertEquals(c1, c2);
+        Assert.assertNotEquals(c1, c3);
+    }
+
+}
