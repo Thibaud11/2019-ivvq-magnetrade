43a870e50fe678da26203b0d8643d44b711038d5
Rialy <rialy.andriamiseza@gmail.com>
2019-04-04 13:54:28 +0200

    magnetController

---
+++ src/main/java/fr/univtlse3/m2dl/magnetrade/magnet/MagnetController.java
@@ -0,0 +1,26 @@
+package fr.univtlse3.m2dl.magnetrade.magnet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/magnet")
+public class MagnetController {
+
+    @Autowired
+    private MagnetService magnetService;
+
+    /**
+     * Method to create a magnet in the back-end
+     *
+     * @param magnet the magnet to create
+     */
+    @PostMapping("/save")
+    public Magnet createMagnet(@RequestBody Magnet magnet) {
+        return magnetService.saveMagnet(magnet);
+    }
+
+}
