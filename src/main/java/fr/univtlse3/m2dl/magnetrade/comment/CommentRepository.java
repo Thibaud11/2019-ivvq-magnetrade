a9e0556b1e5e9d67d8236bd90c6c75533d0716fe
hugo.clems <hugoc.pro@gmail.com>
2019-05-11 19:22:19 +0200

    IT Comment

--- src/main/java/fr/univtlse3/m2dl/magnetrade/comment/CommentRepository.java
+++ src/main/java/fr/univtlse3/m2dl/magnetrade/comment/CommentRepository.java
@@ -3,9 +3,11 @@ package fr.univtlse3.m2dl.magnetrade.comment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, Long> {
 
-    // Empty
+    List<Comment> findAll();
 
 }
